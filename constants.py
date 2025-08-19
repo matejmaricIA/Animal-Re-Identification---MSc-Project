44b4373c9d9e501335fb6f9153dcb142aef6e2e1
matej <matej.maric99@gmail.com>
2025-08-19 15:11:51 +0200

    fixed constants

--- constants.py
+++ constants.py
@@ -36,7 +36,7 @@ SAVE_TEST_DESCRIPTORS_FOLDER = './data/{}/feature_descriptors_test/'
 
 # PCA and GMM Components
 N_COMPONENTS_GMM = 256
-N_COMPONENTS_PCA = 96 # Ovo promijenio zadnje da testiram.
+N_COMPONENTS_PCA = 128 # Ovo promijenio zadnje da testiram.
 
 # Pkl Paths
 PCA_PATH = './data/{}/pca_model_{}.pkl'
--- run_multiple.sh
+++ run_multiple.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #"CowDataset" "GiraffeZebraID" "Giraffes" "HyenaID2022" "IPanda50" "LeopardID2022" "NyalaData" "PolarBearVidID" "SealID" "StripeSpotter" "SeaStarReID2023" "SeaTurtleID" "WhaleSharkID" "BelugaID" "NDD20"
-params=("ATRW" "CowDataset" "IPanda50" "NyalaData" "SealID" "BelugaID" "HyenaID2022" "StripeSpotter" "Giraffes")
+params=("BelugaID" "ATRW" "CowDataset" "IPanda50" "NyalaData" "SealID" "HyenaID2022" "StripeSpotter" "Giraffes")
 for p in "${params[@]}"; do
     echo "Running script with parameter: $p"
     python main.py --ds "$p" --train --method ensamble --save_eval --remove_background --use_geometric_verification --use_lightglue --use_global_embedding --embedding_model megadescriptor-l-384  --w_fisher 1.0 --w_global 1.0
